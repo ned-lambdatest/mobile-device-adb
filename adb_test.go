cb63b8f31e405bf4b21219230016fb3eb2e4e592
ned-lambdatest <85100968+ned-lambdatest@users.noreply.github.com>
2021-08-18 20:26:38 +0530

    Add files via upload

---
+++ adb_test.go
@@ -0,0 +1,21 @@
+package adb
+
+import (
+	"testing"
+
+	"github.com/ned-lambdatest/mobile-device-adb/wire"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetServerVersion(t *testing.T) {
+	s := &MockServer{
+		Status:   wire.StatusSuccess,
+		Messages: []string{"000a"},
+	}
+	client := &Adb{s}
+
+	v, err := client.ServerVersion()
+	assert.Equal(t, "host:version", s.Requests[0])
+	assert.NoError(t, err)
+	assert.Equal(t, 10, v)
+}
---
+++ device_state.go
@@ -0,0 +1,36 @@
+package adb
+
+import "github.com/ned-lambdatest/mobile-device-adb/fixinternal/errors"
+
+// DeviceState represents one of the 3 possible states adb will report devices.
+// A device can be communicated with when it's in StateOnline.
+// A USB device will make the following state transitions:
+// 	Plugged in: StateDisconnected->StateOffline->StateOnline
+// 	Unplugged:  StateOnline->StateDisconnected
+//go:generate stringer -type=DeviceState
+type DeviceState int8
+
+const (
+	StateInvalid DeviceState = iota
+	StateAuthorizing
+	StateUnauthorized
+	StateDisconnected
+	StateOffline
+	StateOnline
+)
+
+var deviceStateStrings = map[string]DeviceState{
+	"":             StateDisconnected,
+	"offline":      StateOffline,
+	"device":       StateOnline,
+	"unauthorized": StateUnauthorized,
+	"authorizing":  StateAuthorizing,
+}
+
+func parseDeviceState(str string) (DeviceState, error) {
+	state, ok := deviceStateStrings[str]
+	if !ok {
+		return StateInvalid, errors.Errorf(errors.ParseError, "invalid device state: %q", state)
+	}
+	return state, nil
+}
